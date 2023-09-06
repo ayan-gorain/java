de71e5c684321f31adee5e962b69a227537d52f1
Ayan Gorain <ayangorain@ayans-macbook-air.local>
2023-09-06 12:07:22 +0530

    first commit

---
+++ src/days100/add_two_no.java
@@ -0,0 +1,12 @@
+package days100;
+import java.util.Scanner;
+public class add_two_no {
+    public static void main(String[]  args){
+        Scanner sc=new Scanner(System.in);
+        System.out.println("enter a");
+        int a=sc.nextInt();
+        System.out.println("enter b");
+        int b=sc.nextInt();
+        System.out.println(a+b);
+    }
+}
---
+++ src/days100/hello_word.java
@@ -0,0 +1,7 @@
+package days100;
+
+public class hello_word {
+    public static void main(String[] args){
+        System.out.println("hello world");
+    }
+}
