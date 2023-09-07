2254e9f9ca8bba843a2439102b411cb3c30ccdfd
ayangorain <ayan.gorain2021@vitbhopal.ac.in>
2023-09-07 19:50:43 +0530

    daily commit

---
+++ src/days100/simple_intersest.java
@@ -0,0 +1,13 @@
+package days100;
+
+public class simple_intersest {
+  public static void main(String[] args){
+      int r=99;
+      int t=8;
+      int p=10000000;
+      int sum=p*r*t;
+      int si=sum/100;
+      System.out.println(si);
+
+    }
+}
